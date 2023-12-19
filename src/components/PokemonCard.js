e5221b0a9526ae55f155cf98e6039a68f1a8e085
tyler kim <yoonsoo.kim11@gmail.com>
2023-12-19 11:01:10 -0500

    completed deliv1

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({poke}) {
+  const [isFront, setIsFront] = useState(true)
+
   return (
     <Card>
       <div>
         <div className="image">
-          <img alt="oh no!" />
+          <img
+           onClick={()=> setIsFront(!isFront)} 
+          src={isFront? poke.sprites.front : poke.sprites.back}
+          alt="oh no!"
+          />
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{poke.name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {poke.hp}
           </span>
         </div>
       </div>
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
-
+const url = 'http://127.0.0.1:3001/pokemon'
+//Create an index displaying Pokemon 'cards' 
+// - Render each Pokemon name, sprite, and hp in a card..oass diwn to collection then to card
 function PokemonPage() {
+  const [pokemon, setPokemon] = useState([]);  
+
+  useEffect(() =>{
+    fetch(url)
+     .then(response => response.json())
+     .then(data => setPokemon(data))
+  },[])
+  
+  
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
@@ -13,7 +24,7 @@ function PokemonPage() {
       <br />
       <Search />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemon={pokemon} />
     </Container>
   );
 }
